d37544e8b164b3f7bd9de5f2f371300613eedc28
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-02-04 02:50:25 +0100

    Added missing files

---
+++ sysABC.Infrastructure/Commands/Users/ChangeRoleUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace sysABC.Infrastructure.Commands.Users
+{
+    public class ChangeRoleUser
+    {
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        public ChangeRoleUser()
+        {
+        }
+    }
+}
---
+++ sysABC.Infrastructure/Migrations/20180203213333_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace sysABC.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    NickName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Role = table.Column<string>(nullable: true),
+                    Salt = table.Column<string>(nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
